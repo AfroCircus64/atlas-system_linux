d42dfbf85d4ec5b0dbb11ac3d033bad24328cabd
Trey Turner <trey.turner1857@gmail.com>
2024-11-13 21:42:39 -0600

    Fixing the headerfile

--- nm_objdump/hnm.h
+++ nm_objdump/hnm.h
@@ -11,7 +11,11 @@
 
 
 void print_symbols(const char *filename);
-void print_symbols_32(int fd, Elf32_Ehdr *ehdr);
-void print_symbols_64(int fd, Elf64_Ehdr *ehdr);
+void print_symbols_32(int fd, Elf32_Ehdr *ehdr, int is_big_endian);
+void print_symbols_64(int fd, Elf64_Ehdr *ehdr, int is_big_endian);
+uint16_t swap16(uint16_t val);
+uint32_t swap32(uint32_t val);
+uint64_t swap64(uint64_t val);
+
 
 #endif
