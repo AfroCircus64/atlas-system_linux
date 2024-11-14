bc6e0fba0cd30b6de87aa243777d009c626f3ddb
Trey Turner <trey.turner1857@gmail.com>
2024-11-13 21:54:42 -0600

    Fixing the compilation errors.

--- nm_objdump/hnm2.c
+++ nm_objdump/hnm2.c
@@ -2,242 +2,258 @@
 
 void print_symbols(const char *filename)
 {
-    int fd;
-    unsigned char e_ident[EI_NIDENT];
+	int fd;
+	unsigned char e_ident[EI_NIDENT];
 
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 	{
-        perror("open");
-        return;
-    }
+		perror("open");
+		return;
+	}
 
-    if (read(fd, e_ident, EI_NIDENT) != EI_NIDENT)
+	if (read(fd, e_ident, EI_NIDENT) != EI_NIDENT)
 	{
-        perror("read");
-        close(fd);
-        return;
-    }
+		perror("read");
+		close(fd);
+		return;
+	}
 
-    if (memcmp(e_ident, ELFMAG, SELFMAG) != 0)
+	if (memcmp(e_ident, ELFMAG, SELFMAG) != 0)
 	{
-        fprintf(stderr, "%s is not an ELF file\n", filename);
-        close(fd);
-        return;
-    }
+		fprintf(stderr, "%s is not an ELF file\n", filename);
+		close(fd);
+		return;
+	}
 
-    int is_big_endian = (e_ident[EI_DATA] == ELFDATA2MSB);
+	int is_big_endian = (e_ident[EI_DATA] == ELFDATA2MSB);
 
-    if (e_ident[EI_CLASS] == ELFCLASS32)
+	if (e_ident[EI_CLASS] == ELFCLASS32)
 	{
-        Elf32_Ehdr ehdr;
-        lseek(fd, 0, SEEK_SET);
+		Elf32_Ehdr ehdr;
+		lseek(fd, 0, SEEK_SET);
 
-        if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr))
+		if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr))
 		{
-            perror("read");
-            close(fd);
-            return;
-        }
-        print_symbols_32(fd, &ehdr, is_big_endian);
-    }
+			perror("read");
+			close(fd);
+			return;
+		}
+		print_symbols_32(fd, &ehdr, is_big_endian);
+	}
 	else if (e_ident[EI_CLASS] == ELFCLASS64)
 	{
-        Elf64_Ehdr ehdr;
-        lseek(fd, 0, SEEK_SET);
+		Elf64_Ehdr ehdr;
+		lseek(fd, 0, SEEK_SET);
 
-        if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr))
+		if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr))
 		{
-            perror("read");
-            close(fd);
-            return;
-        }
-        print_symbols_64(fd, &ehdr, is_big_endian);
-    }
+			perror("read");
+			close(fd);
+			return;
+		}
+		print_symbols_64(fd, &ehdr, is_big_endian);
+	}
 	else
 	{
-        fprintf(stderr, "Unknown ELF class\n");
-    }
+		fprintf(stderr, "Unknown ELF class\n");
+	}
 
-    close(fd);
+	close(fd);
 }
 
 void print_symbols_32(int fd, Elf32_Ehdr *ehdr, int is_big_endian)
 {
-    Elf32_Shdr *shdrs;
-    Elf32_Sym *symtab;
-    char *strtab;
-    int i, j;
-    size_t shdr_size, symtab_size;
+	Elf32_Shdr *shdrs;
+	Elf32_Sym *symtab;
+	char *strtab;
+	int i;
+	size_t shdr_size, symtab_size;
 
-    shdr_size = ehdr->e_shentsize * ehdr->e_shnum;
-    shdrs = malloc(shdr_size);
+	shdr_size = ehdr->e_shentsize * ehdr->e_shnum;
+	shdrs = malloc(shdr_size);
 
-    if (!shdrs)
+	if (!shdrs)
 	{
-        perror("malloc");
-        return;
-    }
+		perror("malloc");
+		return;
+	}
 
-    lseek(fd, ehdr->e_shoff, SEEK_SET);
+	lseek(fd, ehdr->e_shoff, SEEK_SET);
 
-    if (read(fd, shdrs, shdr_size) != shdr_size)
+	ssize_t bytes_read;
+	bytes_read = read(fd, shdrs, shdr_size);
+
+	if (bytes_read < 0 || (size_t)bytes_read != shdr_size)
 	{
-        perror("read");
-        free(shdrs);
-        return;
-    }
+		perror("read");
+		free(shdrs);
+		return;
+	}
 
-    for (i = 0; i < ehdr->e_shnum; i++)
+	for (i = 0; i < ehdr->e_shnum; i++)
 	{
-        if (shdrs[i].sh_type == SHT_SYMTAB)
+		if (shdrs[i].sh_type == SHT_SYMTAB)
 		{
-            symtab_size = shdrs[i].sh_size;
-            symtab = malloc(symtab_size);
+			symtab_size = shdrs[i].sh_size;
+			symtab = malloc(symtab_size);
 
-            if (!symtab)
+			if (!symtab)
 			{
-                perror("malloc");
-                free(shdrs);
-                return;
-            }
+				perror("malloc");
+				free(shdrs);
+				return;
+			}
 
-            lseek(fd, shdrs[i].sh_offset, SEEK_SET);
+			lseek(fd, shdrs[i].sh_offset, SEEK_SET);
 
-            if (read(fd, symtab, symtab_size) != symtab_size)
+			bytes_read = read(fd, symtab, symtab_size);
+
+			if (bytes_read < 0 || (size_t)bytes_read != symtab_size)
 			{
-                perror("read");
-                free(symtab);
-                free(shdrs);
-                return;
-            }
+				perror("read");
+				free(symtab);
+				free(shdrs);
+				return;
+			}
 
-            strtab = malloc(shdrs[shdrs[i].sh_link].sh_size);
+			strtab = malloc(shdrs[shdrs[i].sh_link].sh_size);
 
-            if (!strtab)
+			if (!strtab)
 			{
-                perror("malloc");
-                free(symtab);
-                free(shdrs);
-                return;
-            }
+				perror("malloc");
+				free(symtab);
+				free(shdrs);
+				return;
+			}
 
-            lseek(fd, shdrs[shdrs[i].sh_link].sh_offset, SEEK_SET);
+			lseek(fd, shdrs[shdrs[i].sh_link].sh_offset, SEEK_SET);
 
-            if (read(fd, strtab, shdrs[shdrs[i].sh_link].sh_size) != shdrs[shdrs[i].sh_link].sh_size)
+			if (read(fd, strtab, shdrs[shdrs[i].sh_link].sh_size) != shdrs[shdrs[i].sh_link].sh_size)
 			{
-                perror("read");
-                free(strtab);
-                free(symtab);
-                free(shdrs);
-                return;
-            }
+				perror("read");
+				free(strtab);
+				free(symtab);
+				free(shdrs);
+				return;
+			}
 
-            for (j = 0; j < symtab_size / sizeof(Elf32_Sym); j++)
+			size_t num_symbols = symtab_size / sizeof(Elf32_Sym);
+
+			for (size_t j = 0; j < num_symbols; j++)
 			{
-                uint32_t value = symtab[j].st_value;
+				uint32_t value = symtab[j].st_value;
 
-                if (is_big_endian)
+				if (is_big_endian)
 				{
-                    value = swap32(value);
-                }
-                printf("%08x %c %s\n", value, ELF32_ST_TYPE(symtab[j].st_info), &strtab[symtab[j].st_name]);
-            }
+					value = swap32(value);
+				}
+				printf("%08x %c %s\n", value, ELF32_ST_TYPE(symtab[j].st_info), &strtab[symtab[j].st_name]);
+			}
 
-            free(strtab);
-            free(symtab);
-        }
-    }
+			free(strtab);
+			free(symtab);
+		}
+	}
 
-    free(shdrs);
+	free(shdrs);
 }
 
 void print_symbols_64(int fd, Elf64_Ehdr *ehdr, int is_big_endian)
 {
-    Elf64_Shdr *shdrs;
-    Elf64_Sym *symtab;
-    char *strtab;
-    int i, j;
-    size_t shdr_size, symtab_size;
+	Elf64_Shdr *shdrs;
+	Elf64_Sym *symtab;
+	char *strtab;
+	int i;
+	size_t shdr_size, symtab_size;
 
-    shdr_size = ehdr->e_shentsize * ehdr->e_shnum;
-    shdrs = malloc(shdr_size);
+	shdr_size = ehdr->e_shentsize * ehdr->e_shnum;
+	shdrs = malloc(shdr_size);
 
-    if (!shdrs)
+	if (!shdrs)
 	{
-        perror("malloc");
-        return;
-    }
+		perror("malloc");
+		return;
+	}
 
-    lseek(fd, ehdr->e_shoff, SEEK_SET);
+	lseek(fd, ehdr->e_shoff, SEEK_SET);
 
-    if (read(fd, shdrs, shdr_size) != shdr_size)
+	ssize_t bytes_read;
+	bytes_read = read(fd, shdrs, shdr_size);
+
+	if (bytes_read < 0 || (size_t)bytes_read != shdr_size)
 	{
-        perror("read");
-        free(shdrs);
-        return;
-    }
+		perror("read");
+		free(shdrs);
+		return;
+	}
 
-    for (i = 0; i < ehdr->e_shnum; i++)
+	for (i = 0; i < ehdr->e_shnum; i++)
 	{
-        if (shdrs[i].sh_type == SHT_SYMTAB)
+		if (shdrs[i].sh_type == SHT_SYMTAB)
 		{
-            symtab_size = shdrs[i].sh_size;
-            symtab = malloc(symtab_size);
+			symtab_size = shdrs[i].sh_size;
+			symtab = malloc(symtab_size);
 
-            if (!symtab)
+			if (!symtab)
 			{
-                perror("malloc");
-                free(shdrs);
-                return;
-            }
+				perror("malloc");
+				free(shdrs);
+				return;
+			}
 
-            lseek(fd, shdrs[i].sh_offset, SEEK_SET);
+			lseek(fd, shdrs[i].sh_offset, SEEK_SET);
 
-            if (read(fd, symtab, symtab_size) != symtab_size)
+			bytes_read = read(fd, symtab, symtab_size);
+
+			if (bytes_read < 0 || (size_t)bytes_read != symtab_size)
 			{
-                perror("read");
-                free(symtab);
-                free(shdrs);
-                return;
-            }
+				perror("read");
+				free(symtab);
+				free(shdrs);
+				return;
+			}
 
-            strtab = malloc(shdrs[shdrs[i].sh_link].sh_size);
+			strtab = malloc(shdrs[shdrs[i].sh_link].sh_size);
 
-            if (!strtab)
+			if (!strtab)
 			{
-                perror("malloc");
-                free(symtab);
-                free(shdrs);
-                return;
-            }
+				perror("malloc");
+				free(symtab);
+				free(shdrs);
+				return;
+			}
 
-            lseek(fd, shdrs[shdrs[i].sh_link].sh_offset, SEEK_SET);
+			lseek(fd, shdrs[shdrs[i].sh_link].sh_offset, SEEK_SET);
 
-            if (read(fd, strtab, shdrs[shdrs[i].sh_link].sh_size) != shdrs[shdrs[i].sh_link].sh_size)
+			bytes_read = read(fd, strtab, shdrs[shdrs[i].sh_link].sh_size);
+
+			if (bytes_read < 0 || (size_t)bytes_read != shdrs[shdrs[i].sh_link].sh_size)
 			{
-                perror("read");
-                free(strtab);
-                free(symtab);
-                free(shdrs);
-                return;
-            }
+				perror("read");
+				free(strtab);
+				free(symtab);
+				free(shdrs);
+				return;
+			}
 
-            for (j = 0; j < symtab_size / sizeof(Elf64_Sym); j++)
+			size_t num_symbols = symtab_size / sizeof(Elf64_Sym);
+
+			for (size_t j = 0; j < num_symbols; j++)
 			{
-                uint64_t value = symtab[j].st_value;
+				uint64_t value = symtab[j].st_value;
 
-                if (is_big_endian)
+				if (is_big_endian)
 				{
-                    value = swap64(value);
-                }
-                printf("%016lx %c %s\n", value, ELF64_ST_TYPE(symtab[j].st_info), &strtab[symtab[j].st_name]);
-            }
+					value = swap64(value);
+				}
+				printf("%016lx %c %s\n", value, ELF64_ST_TYPE(symtab[j].st_info), &strtab[symtab[j].st_name]);
+			}
 
-            free(strtab);
-            free(symtab);
-        }
-    }
+			free(strtab);
+			free(symtab);
+		}
+	}
 
-    free(shdrs);
+	free(shdrs);
 }
